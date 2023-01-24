4913882622d627a6b2d72001346be220d1653a2f
Lennie Nurse <lnursej@gmail.com>
2023-01-24 13:12:06 -0500

    " updated"

--- src/Components/Song.js
+++ src/Components/Song.js
@@ -1,5 +1,26 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
-export default function Song() {
-  return <div>Tuner</div>
+export default function Song({ song }) {
+  return (
+    <div className="tuner_song">
+      <tr>
+        <td>
+          {song.is_favorite ? (
+            <span>⭐️</span>
+          ) : (
+            <span>&nbsp; &nbsp; &nbsp;</span>
+          )}
+        </td>
+        <td>
+          <a href={song.url} target="_blank" rel="noreferrer">
+            {song.name}
+          </a>
+        </td>
+        <td>
+          <Link to={`/songs/${song.id}`}> </Link>
+        </td>
+      </tr>
+    </div>
+  )
 }
