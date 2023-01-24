a6d1ab3495d84bcb3feec9a8632567e85b687952
Lennie Nurse <lnursej@gmail.com>
2023-01-24 11:43:13 -0500

    "updated Form"

--- src/Components/SongsEditForm.js
+++ src/Components/SongsEditForm.js
@@ -1,6 +1,52 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { Form } from "react-bootstrap"
+import axios from "axios"
+import { useNavigate, useParams } from "react-router-dom"
+
+const API = process.env.REACT_APP_API_URL
 
 export default function SongsEditForm() {
+  const { id } = useParams
+  const navigate = useNavigate
+
+  const [song, setSong] = useState({
+    name: "",
+    artist: "",
+    album: "",
+    is_favorite: false,
+    time: "",
+  })
+
+  const updatedSongs = (updateSongs) => {
+    axios
+      .put(`${API}/songs/${id}`, updateSongs)
+      .then(
+        () => {
+          navigate(`/songs/${id}`)
+        },
+        (error) => console.error(error)
+      )
+      .catch((c) => console.warn(`catch`, c))
+  }
+
+  const handleTextChange = (e) => {
+    setSong({ ...song, [e.target.id]: e.target.value })
+  }
+
+  const handleCheckboxChange = () => {
+    setSong({ ...song, is_favortie: !song.is_favorite })
+  }
+
+  useEffect(() => {
+    axios.get(`${API}/songs/${id}`).then((res) => setSong(res.data))((error) =>
+      navigate(`/not-found`)
+    )
+  }, [id, navigate])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    updatedSongs(song, id)
+  }
   return (
     <div className="edit_form">
       <Form onsubmit={handleSubmit}>
