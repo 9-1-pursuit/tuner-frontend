354fe7464bc0b3ac1234b6870b99b07d9d5ea4d4
Lennie Nurse <lnursej@gmail.com>
2023-01-26 02:25:27 -0500

    "update"

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ import Error from "./Pages/Error"
 // COMPONENTS
 import NavBar from "./Components/NavBar"
 import Footer from "./Components/Footer"
+import Artists from "./Components/Artists"
+import ArtistForm from "./Components/ArtistForm"
+import Artist from "./Components/Artist"
 
 function App() {
   return (
@@ -24,6 +27,11 @@ function App() {
           <Route path="/songs/new" element={<New />} />
           <Route path="/songs/:id" element={<Show />} />
           <Route path="/songs/:id/edit" element={<Edit />} />
+          <Route path="artists">
+            <Route path="artists/:id" element={<Artist />} />
+            <Route path index element={<Artists />} />
+            <Route path="edit" element={ArtistForm} />
+          </Route>
           <Route path="*" element={<Error />} />
         </Routes>
         <Footer />
--- src/Components/Artists.js
+++ src/Components/Artists.js
@@ -1,5 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
+
+// import { useState, useEffect } from "react"
+// import axios from "axios"
+const API = process.env.REACT_APP_API_URL
 
 export default function Artists() {
+  //   const [artist, setArtist] = useState([])
   return <div>artists</div>
 }
